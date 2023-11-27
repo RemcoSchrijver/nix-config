26fa770766285c68da3da28339b64dda7951eab5
RemcoSchrijver <remco.schrijver@hotmail.com>
2023-11-27 16:29:53 +0100

    Added uair

--- home-manager/apps/uair/uair.nix
+++ home-manager/apps/uair/uair.nix
@@ -4,8 +4,13 @@
 {
     home.packages = [
         pkgs.uair 
+        pkgs.yad
     ];
 
 
     xdg.configFile."uair".source = ./dotfiles;
+
+    home.shellAliases = {
+        uair = "uair | yad --progress --no-buttons --css=\"* { font-size: 80px; }\"";
+    };
 }
