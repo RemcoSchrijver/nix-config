54fdb5a0013eb9dd745dbe1c6e0ff9ed80b8522b
Remco Schrijver <remco.schrijver@hotmail.com>
2023-12-17 19:59:21 +0100

    Added inkscape and some options for the laptop

--- home-manager/apps/editors.nix
+++ home-manager/apps/editors.nix
@@ -3,5 +3,6 @@
 {
   home.packages = [
     pkgs.drawio
+    pkgs.inkscape
   ];
 }
--- nixos/laptop/hardware-configuration.nix
+++ nixos/laptop/hardware-configuration.nix
@@ -12,7 +12,7 @@
   boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
-  boot.extraModulePackages = [ ];
+  boot.extraModulePackages = [ config.boot.kernelPackages.lenovo-legion-module ];
 
   fileSystems."/" =
     {
