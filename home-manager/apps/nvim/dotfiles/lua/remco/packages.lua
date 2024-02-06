219862d56d07524a7279de84b8bce65ad5b54ac6
Remco Schrijver <remco.schrijver@hotmail.com>
2024-02-06 15:36:18 +0100

    Copying instead of symlinking works better, also changed the neovim theme

--- home-manager/apps/nvim/dotfiles/lua/remco/packages.lua
+++ home-manager/apps/nvim/dotfiles/lua/remco/packages.lua
@@ -20,12 +20,12 @@ local plugins = {
 		},
 	},
 	{
-		"folke/tokyonight.nvim",
+		"bluz71/vim-moonfly-colors",
 		lazy = false, -- make sure we load this during startup if it is your main colorscheme
 		priority = 1000, -- make sure to load this before all the other start plugins
 		config = function()
 			-- load the colorscheme here
-			vim.cmd([[colorscheme tokyonight]])
+			vim.cmd([[colorscheme moonfly]])
 		end,
 	},
 	{
---
+++ scripts/copy_neovim_config.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+rm -r ~/.config/nvim/*
+cp -r ../home-manager/apps/nvim/dotfiles/* ~/.config/nvim/
