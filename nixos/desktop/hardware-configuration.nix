404dd2f4421d0ef4387690e61f9332e4fb019c1c
RemcoSchrijver <remco.schrijver@hotmail.com>
2024-03-20 23:11:44 +0100

    changed some hyprland options

--- nixos/desktop/hardware-configuration.nix
+++ nixos/desktop/hardware-configuration.nix
@@ -9,10 +9,14 @@
       (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod" ];
-  boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-amd" ];
-  boot.extraModulePackages = [ ];
+  boot = {
+      initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod" ];
+      initrd.kernelModules = [ ];
+      kernelModules = [ "kvm-amd" ];
+      kernelParams = [ "nvidia.NVreg_PreserveVideoMemoryAllocations=1" ];
+      extraModulePackages = [ ];
+  };
+
 
   fileSystems."/" =
     {
