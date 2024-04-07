a271fac1eb3e585387fccfb3b21e07663c6cf7e2
RemcoSchrijver <remco.schrijver@hotmail.com>
2024-04-07 21:24:43 +0200

    Added power save options for thinkpad

--- nixos/thinkpad/configuration.nix
+++ nixos/thinkpad/configuration.nix
@@ -49,6 +49,40 @@
     };
   };
 
+  # tlp settings so we are not using performance mode in battery.
+  # Also disable the power profiles daemon due to incompatibility.
+  services.power-profiles-daemon.enable = false;
+  services.tlp = {
+    enable = true;
+    settings = {
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+
+      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+
+      PLATFORM_PROFILE_ON_AC = "performance";
+      PLATFORM_PROFILE_ON_BAT = "low-power";
+
+      CPU_BOOST_ON_AC = 1;
+      CPU_BOOST_ON_BAT = 0;
+
+      CPU_HWP_DYN_BOOST_ON_AC = 1;
+      CPU_HWP_DYN_BOOST_ON_BAT = 0;
+
+      #Optional helps save long term battery health
+      START_CHARGE_THRESH_BAT0 = 60; # 60 and below it starts to charge
+      STOP_CHARGE_THRESH_BAT0 = 90; # 90 and above it stops charging
+
+    };
+  };
+
+  # Powertop for extra management possibilities
+  powerManagement.powertop.enable = true;
+
+  # thermald to ensure that temperatures on this thin thinkpad stay sane 
+  services.thermald.enable = true;
+
   # Set your time zone.
   time.timeZone = "Europe/Amsterdam";
 
