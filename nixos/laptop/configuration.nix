86e826f4166d2939a55bbda2f2ae3284f5d8878f
Remco Schrijver <remco.schrijver@hotmail.com>
2024-04-13 18:14:36 +0200

    Added fixes for wireguard

--- nixos/laptop/configuration.nix
+++ nixos/laptop/configuration.nix
@@ -29,6 +29,9 @@
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
+  # fwupd for updating firmware of internals
+  services.fwupd.enable = true;
+
   # Enable Bluetooth
   hardware.bluetooth.enable = true;
   hardware.bluetooth.powerOnBoot = true;
--- nixos/shared/default.nix
+++ nixos/shared/default.nix
@@ -107,15 +107,16 @@
     firewall = {
       # if packets are still dropped, they will show up in dmesg
       logReversePathDrops = true;
+      checkReversePath = "loose";
       # wireguard trips rpfilter up
-      extraCommands = ''
-        ip46tables -t mangle -I nixos-fw-rpfilter -p udp -m udp --sport 51820 -j RETURN
-        ip46tables -t mangle -I nixos-fw-rpfilter -p udp -m udp --dport 51820 -j RETURN
-      '';
-      extraStopCommands = ''
-        ip46tables -t mangle -D nixos-fw-rpfilter -p udp -m udp --sport 51820 -j RETURN || true
-        ip46tables -t mangle -D nixos-fw-rpfilter -p udp -m udp --dport 51820 -j RETURN || true
-      '';
+      # extraCommands = ''
+      #   ip46tables -t mangle -I nixos-fw-rpfilter -p udp -m udp --sport 63941 -j RETURN
+      #   ip46tables -t mangle -I nixos-fw-rpfilter -p udp -m udp --dport 63941 -j RETURN
+      # '';
+      # extraStopCommands = ''
+      #   ip46tables -t mangle -D nixos-fw-rpfilter -p udp -m udp --sport 63941 -j RETURN || true
+      #   ip46tables -t mangle -D nixos-fw-rpfilter -p udp -m udp --dport 63941 -j RETURN || true
+      # '';
     };
   };
 
