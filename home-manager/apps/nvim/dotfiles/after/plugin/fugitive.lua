428be1c96d6f4ee8b096eb244d73befbc9f03770
RemcoSchrijver <remco.schrijver@hotmail.com>
2024-09-05 16:41:28 +0200

    Added some fugitive keybindings for neovim

--- home-manager/apps/nvim/dotfiles/after/plugin/fugitive.lua
+++ home-manager/apps/nvim/dotfiles/after/plugin/fugitive.lua
@@ -1,1 +1,5 @@
-vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+-- Keybindings for git, mirroring aliases in the commandline.
+vim.keymap.set("n", "<leader>gs", "<cmd>Git<CR>")
+vim.keymap.set("n", "<leader>ga", "<cmd>Git add --all<CR>")
+vim.keymap.set("n", "<leader>gc", "<cmd>Git commit<CR>")
+vim.keymap.set("n", "<leader>gp", "<cmd>Git push<CR>")
