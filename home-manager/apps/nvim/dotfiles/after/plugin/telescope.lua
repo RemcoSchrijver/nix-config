372bcf8a8fe4ca8fea99d37a5468f359cfafccf4
RemcoSchrijver <remco.schrijver@hotmail.com>
2024-09-09 16:02:45 +0200

    Added telescope icons and removed tmux navigator for neovim

--- home-manager/apps/nvim/dotfiles/after/plugin/telescope.lua
+++ home-manager/apps/nvim/dotfiles/after/plugin/telescope.lua
@@ -2,5 +2,37 @@ local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<leader>gf', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") })
+    builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
+
+require 'nvim-web-devicons'.setup {
+    -- globally enable different highlight colors per icon (default to true)
+    -- if set to false all icons will have the default icon's color
+    color_icons = true,
+
+    -- globally enable default icons (default to false)
+    -- will get overriden by `get_icons` option
+    default = true,
+
+    -- globally enable "strict" selection of icons - icon will be looked up in
+    -- different tables, first by filename, and if not found by extension; this
+    -- prevents cases when file doesn't have any extension but still gets some icon
+    -- because its name happened to match some extension (default to false)
+    strict = true,
+
+    override_by_filename = {
+        [".gitignore"] = {
+            icon = "",
+            color = "#f1502f",
+            name = "Gitignore"
+        }
+    },
+
+    override_by_extension = {
+        ["log"] = {
+            icon = "",
+            color = "#81e043",
+            name = "Log"
+        }
+    },
+}
--- home-manager/apps/nvim/dotfiles/lua/remco/packages.lua
+++ home-manager/apps/nvim/dotfiles/lua/remco/packages.lua
@@ -1,77 +1,61 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 
 vim.opt.rtp:prepend(lazypath)
 
 local plugins = {
-	{
-		'nvim-telescope/telescope.nvim', tag = '0.1.3',
-		dependencies = {
-			'nvim-lua/plenary.nvim'
-		},
-	},
-	{
+    {
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.3',
+        dependencies = {
+            'nvim-lua/plenary.nvim',
+            'nvim-tree/nvim-web-devicons'
+        },
+    },
+    {
         -- Theme
         "rose-pine/neovim",
         name = "rose-pine",
-		lazy = false, -- make sure we load this during startup if it is your main colorscheme
-		priority = 1000, -- make sure to load this before all the other start plugins
-	},
-	{
-		'nvim-treesitter/nvim-treesitter',
-		build = ":TSUpdate",
-	},
-	{
-		'ThePrimeagen/harpoon',
-		dependencies = {
-			'nvim-lua/plenary.nvim'
-		}
-	},
-	{
-		'mbbill/undotree',
-	},
-    {
-        -- tmux plugins 
-        'christoomey/vim-tmux-navigator',
-          cmd = {
-            "TmuxNavigateLeft",
-            "TmuxNavigateDown",
-            "TmuxNavigateUp",
-            "TmuxNavigateRight",
-            "TmuxNavigatePrevious",
-          },
-          keys = {
-            { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
-            { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
-            { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
-            { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
-            { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
-          },
+        lazy = false,    -- make sure we load this during startup if it is your main colorscheme
+        priority = 1000, -- make sure to load this before all the other start plugins
     },
-	{
-		'tpope/vim-fugitive',
-	},
     {
-        {'williamboman/mason.nvim'},
-        {'williamboman/mason-lspconfig.nvim'},
+        'nvim-treesitter/nvim-treesitter',
+        build = ":TSUpdate",
+    },
+    {
+        'ThePrimeagen/harpoon',
+        dependencies = {
+            'nvim-lua/plenary.nvim'
+        }
+    },
+    {
+        'mbbill/undotree',
+    },
+    {
+        'tpope/vim-fugitive',
+    },
+    {
+        { 'williamboman/mason.nvim' },
+        { 'williamboman/mason-lspconfig.nvim' },
 
-        {'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
-        {'neovim/nvim-lspconfig'},
-        {'L3MON4D3/LuaSnip'},
-        {'hrsh7th/cmp-path'},
-        {'hrsh7th/cmp-nvim-lsp'},
-        {'hrsh7th/nvim-cmp'},
+        { 'VonHeikemen/lsp-zero.nvim',        branch = 'v3.x' },
+        { 'neovim/nvim-lspconfig' },
+        { 'L3MON4D3/LuaSnip' },
+        { 'hrsh7th/cmp-path' },
+        { 'hrsh7th/cmp-nvim-lsp' },
+        { 'hrsh7th/nvim-cmp' },
         -- Help with Vimtex completions
-        {'hrsh7th/cmp-omni'},
+        { 'hrsh7th/cmp-omni' },
     },
     {
         "folke/which-key.nvim",
@@ -89,7 +73,7 @@ local plugins = {
     {
         'scalameta/nvim-metals',
         dependencies = {
-			'nvim-lua/plenary.nvim'
+            'nvim-lua/plenary.nvim'
         }
     },
     {
@@ -110,4 +94,4 @@ local opts = {
     lockfile = '~/nix-config/home-manager/apps/nvim/dotfiles/lazy-lock.json'
 }
 
-require("lazy").setup(plugins,opts)
+require("lazy").setup(plugins, opts)
