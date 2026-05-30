f087b277a798bef552cf8ff696a5fb36996166ea
RemcoSchrijver <remco.schrijver@hotmail.com>
2026-05-30 18:45:55 +0200

    fix: Drop HM settings for ff cause it was problematic

--- home-manager/apps/browsers.nix
+++ home-manager/apps/browsers.nix
@@ -3,19 +3,7 @@
 {
   # Firefox
   programs.firefox = {
-    configPath = "${config.xdg.configHome}/mozilla/firefox";
     enable = true;
-    profiles.remco = {
-      settings = {
-        "general.smoothScroll" = true;
-
-        # Don't have to be warned on aboutConfig.
-        "browser.aboutConfig.showWarning" = false;
-
-        # Turn off remember password prompts.
-        "signon.remeberSignons" = false;
-      };
-    };
   };
 
   # Chromium
