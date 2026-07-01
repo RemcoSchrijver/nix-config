dfb2ba162e964b9b7eedf31274e35a44c1b5a64a
Remco Schrijver <remco.schrijver@hotmail.com>
2026-07-01 15:43:02 +0200

    fix: Notifications for java no longer trigger everytime

--- home-manager/apps/nvim/dotfiles/lsp/jdtls.lua
+++ home-manager/apps/nvim/dotfiles/lsp/jdtls.lua
@@ -1,19 +1,8 @@
+local notified_runtimes = false
+local notified_lombok = false
+
 local runtimes_str = vim.env.NIX_JAVA_RUNTIMES
 local lombok_jar = vim.env.NIX_LOMBOK
-
-if not runtimes_str then
-    vim.notify(
-        "NIX_JAVA_RUNTIMES is not set. Install the Nix java module.",
-        vim.log.levels.ERROR
-    )
-end
-
-if not lombok_jar then
-    vim.notify(
-        "Lombok is not set, Lombok based methods will not be found. Install the Nix java module.",
-        vim.log.levels.WARN
-    )
-end
 
 local runtimes = {}
 if runtimes_str then
@@ -38,7 +27,7 @@ return {
         ('--jvm-arg=-javaagent:%s'):format(lombok_jar)
     },
 
-    root_markers = { 'pom.xml' },
+    root_markers = { 'pom.xml', '.git' },
 
     settings = {
         java = {
@@ -47,4 +36,23 @@ return {
             },
         },
     },
+
+    on_attach = function(client, bufnr)
+        if not notified_runtimes and not runtimes_str then
+            vim.notify(
+                "NIX_JAVA_RUNTIMES is not set. Install the Nix java module.",
+                vim.log.levels.ERROR
+            )
+            notified_runtimes = true;
+        end
+
+        if not notified_lombok and not lombok_jar then
+            vim.notify(
+                "Lombok is not set, Lombok based methods will not be found. Install the Nix java module.",
+                vim.log.levels.WARN
+            )
+            notified_lombok = true;
+        end
+    end
+
 }
